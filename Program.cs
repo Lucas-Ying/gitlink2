0608aca22098b4de68aafa740580f65862dedae9
Lucas Ying <lucas.ying@aderant.com>
2018-06-13 14:53:34 +1200

    init

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gitlink2 {
+    class Program {
+        static void Main(string[] args)
+        {
+            Debugger.Launch();
+            Console.WriteLine();
+        }
+    }
+}
